cd242ac07ed66d6e01735cc7a71485711cdb24a8
Aure-M <marcaurelalioza@gmail.com>
2022-10-11 20:47:22 +0200

    initial commit

---
+++ prj_utils.py
@@ -0,0 +1,155 @@
+import pandas as pd 
+import matplotlib.pyplot as plt
+import plotly.express as px
+import seaborn as sn
+import streamlit as st
+import folium
+from datetime import datetime
+
+links = {
+    2021:"https://static.data.gouv.fr/resources/demandes-de-valeurs-foncieres/20220408-143516/valeursfoncieres-2021.txt",
+    2020:"https://static.data.gouv.fr/resources/demandes-de-valeurs-foncieres/20220408-143240/valeursfoncieres-2020.txt",
+    2019:"https://static.data.gouv.fr/resources/demandes-de-valeurs-foncieres/20220408-142854/valeursfoncieres-2019.txt"
+}
+
+@st.cache(suppress_st_warning=True)
+def fetchData(year):
+    data = pd.read_csv("./sample2.csv")
+    return data
+
+
+@st.cache(suppress_st_warning=True)
+def cleanData(data):
+    if data["date_mutation"].dtype==type(object):
+        data["date_mutation"] = [datetime.strptime(str(i), '%Y-%m-%d') for i in data["date_mutation"]]
+    data = data.loc[(~data["latitude"].isna()) & (~data["longitude"].isna())&(~data["type_local"].isna())&(~data["valeur_fonciere"].isna())]
+    return data
+
+
+@st.cache(suppress_st_warning=True)
+def defineKeys(data):
+    return {
+        "nature_mutation": list(data["nature_mutation"].value_counts().index),
+        "valeur_fonciere": [data["valeur_fonciere"].min(), data["valeur_fonciere"].max()],
+        "nom_commune": list(data["nom_commune"].value_counts().index),
+        "nombre_lots": [data["nombre_lots"].min(),data["nombre_lots"].max()],
+        "type_local": list(data["type_local"].value_counts().index),
+        "surface_reelle_bati": [data["surface_reelle_bati"].min(), data["surface_reelle_bati"].max()],
+        "nombre_pieces_principales": [data["nombre_pieces_principales"].min(),data["nombre_pieces_principales"].max()],
+        "surface_terrain": [data["surface_terrain"].min(), data["surface_terrain"].max()]
+    }
+
+@st.cache(suppress_st_warning=True)
+def filter(data,dateMutation,natureMutation,valeurF,commune,typeLocal,surfaceT,nbrePieces):
+    print(dateMutation)
+    data = data.loc[
+        (data["nature_mutation"].isin(natureMutation))&
+        (data["nom_commune"].isin(commune))&
+        (data["type_local"].isin(typeLocal))&
+        (data["valeur_fonciere"].between(valeurF[0],valeurF[1]))&
+        (data["date_mutation"].between(dateMutation[0],dateMutation[1]))&
+        (data["surface_terrain"].between(surfaceT[0],surfaceT[1]))&
+        (data["nombre_pieces_principales"].between(nbrePieces[0],nbrePieces[1]))
+        
+    ]
+    return data
+
+@st.cache(suppress_st_warning=True)
+def convert_df(df):
+    return df.to_csv().encode('utf-8')
+
+def add_categorical_legend(folium_map, title, color_by_label):
+    
+    legend_categories = ""     
+    for label, color in color_by_label.items():
+        legend_categories += f"<li><span style='background:{color}'></span>{label}</li>"
+        
+    legend_html = f"""
+    <div id='maplegend' class='maplegend'>
+      <div class='legend-title'>{title}</div>
+      <div class='legend-scale'>
+        <ul class='legend-labels'>
+        {legend_categories}
+        </ul>
+      </div>
+    </div>
+    """
+    script = f"""
+        <script type="text/javascript">
+        var oneTimeExecution = (function() {{
+                    var executed = false;
+                    return function() {{
+                        if (!executed) {{
+                             var checkExist = setInterval(function() {{
+                                       if ((document.getElementsByClassName('leaflet-top leaflet-right').length) || (!executed)) {{
+                                          document.getElementsByClassName('leaflet-top leaflet-right')[0].style.display = "flex"
+                                          document.getElementsByClassName('leaflet-top leaflet-right')[0].style.flexDirection = "column"
+                                          document.getElementsByClassName('leaflet-top leaflet-right')[0].innerHTML += `{legend_html}`;
+                                          clearInterval(checkExist);
+                                          executed = true;
+                                       }}
+                                    }}, 100);
+                        }}
+                    }};
+                }})();
+        oneTimeExecution()
+        </script>
+      """
+   
+
+    css = """
+
+    <style type='text/css'>
+      .maplegend {
+        z-index:9999;
+        float:right;
+        background-color: rgba(255, 255, 255, 1);
+        border-radius: 5px;
+        border: 2px solid #bbb;
+        padding: 10px;
+        font-size:12px;
+        positon: relative;
+      }
+      .maplegend .legend-title {
+        text-align: left;
+        margin-bottom: 5px;
+        font-weight: bold;
+        font-size: 90%;
+        }
+      .maplegend .legend-scale ul {
+        margin: 0;
+        margin-bottom: 5px;
+        padding: 0;
+        float: left;
+        list-style: none;
+        }
+      .maplegend .legend-scale ul li {
+        font-size: 80%;
+        list-style: none;
+        margin-left: 0;
+        line-height: 18px;
+        margin-bottom: 2px;
+        }
+      .maplegend ul.legend-labels li span {
+        display: block;
+        float: left;
+        height: 16px;
+        width: 30px;
+        margin-right: 5px;
+        margin-left: 0;
+        border: 0px solid #ccc;
+        }
+      .maplegend .legend-source {
+        font-size: 80%;
+        color: #777;
+        clear: both;
+        }
+      .maplegend a {
+        color: #777;
+        }
+    </style>
+    """
+
+    folium_map.get_root().header.add_child(folium.Element(script + css))
+
+    return folium_map
---
+++ project.py
@@ -0,0 +1,99 @@
+import pandas as pd 
+import matplotlib.pyplot as plt
+import plotly.express as px
+import seaborn as sn
+import streamlit as st
+from datetime import date,datetime
+from prj_utils import cleanData, convert_df, defineKeys, fetchData, filter
+from streamlit_folium import st_folium
+import folium
+    
+
+
+
+year = 2017
+#----------------------------
+df = fetchData(year)
+df = cleanData(df)
+keys = defineKeys(df)
+
+#----------------------------
+
+
+
+with st.sidebar:
+    option = st.selectbox(
+    '',
+    ('2021 analysis', '2020 analysis', 'Filter feature'))
+
+    if option == 'Filter feature':
+        st.subheader("Choose your preference:")
+        dateMutation = st.slider(
+            "Select a range for your research",
+            datetime(year,1,1),datetime(year,12,31),
+            value=(datetime(year,1,1), datetime(year,5,1)),
+        )
+        natureMutation = st.multiselect(
+            'Choose the types of transactions',
+            keys["nature_mutation"],
+            keys["nature_mutation"][0]
+        )
+        valeurF = st.slider(
+            "Lands value :",
+            keys["valeur_fonciere"][0],keys["valeur_fonciere"][1],
+            value=(keys["valeur_fonciere"][0], keys["valeur_fonciere"][1]),
+            step = (keys["valeur_fonciere"][1]-keys["valeur_fonciere"][0])/50
+        ) 
+        commune = st.multiselect(
+            'Choose the municipalities that you interested in:',
+            keys["nom_commune"],
+            keys["nom_commune"][:2]
+        )
+        typeLocal = st.multiselect(
+            'Choose the types of Property : ',
+            keys["type_local"],
+            keys["type_local"][:2]
+        )
+        surfaceT = st.slider(
+            "Lands surface :",
+            keys["surface_terrain"][0],keys["surface_terrain"][1],
+            value=(keys["surface_terrain"][0], keys["surface_terrain"][1]),
+            step = (keys["surface_terrain"][1]-keys["surface_terrain"][0])/50
+        )
+        nbrePieces = st.slider(
+            "Number of main rooms : ",
+            keys["nombre_pieces_principales"][0],keys["nombre_pieces_principales"][1],
+            value=(keys["nombre_pieces_principales"][0], keys["nombre_pieces_principales"][1]),
+            step = (keys["nombre_pieces_principales"][1]-keys["nombre_pieces_principales"][0])/50
+        )
+
+if option == 'Filter feature':
+    st.write("# Filter feature")
+    limit = 300 # Limit of properties per pages
+    filtered = filter(df,dateMutation,natureMutation,valeurF,commune,typeLocal,surfaceT,nbrePieces)
+    page = st.select_slider(
+        'Select the page',
+        options = range(int(len(df)/limit)),
+        value = 0
+    )
+    m = folium.Map(location=[48.856614,2.3522219]) # Paris location
+    colors = {"Maison":'lightgreen',"Appartement":'lightblue',"Dépendance":'red',"Local industriel. commercial ou assimilé":'orange'}
+    st.write(colors)
+    for ind, lat, lon, typ, val in filtered[["latitude","longitude","type_local","valeur_fonciere"]][limit*page:limit*(page+1)].itertuples():     
+        folium.Marker(
+            [lat,lon],
+            icon=folium.Icon(icon="glyphicon-map-marker",color=colors[typ]),
+            tooltip = "{} €".format(val)
+        ).add_to(m)
+    st_data = st_folium(m, width = 800,height=600)
+    
+    csv = convert_df(filtered)
+
+    st.download_button(
+        label="Download data as CSV",
+        data=csv,
+        file_name='large_df.csv',
+        mime='text/csv',
+    )
+
+    
